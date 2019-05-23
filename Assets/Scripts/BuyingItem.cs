931deabc2f90577b70bafa65183a4fe2c6f72191
lheartl <lheartl@nate.com>
2019-05-23 23:46:00 +0900

    아이템 버튼제작
    아이템 버튼

---
+++ Assets/Scripts/BuyingItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyingItem : MonoBehaviour
+{
+    public long item_price = 100;
+
+    public Image item_img;
+    public Slider price_slider;
+
+    private void Start()
+    {
+        price_slider.maxValue = item_price;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Color temp = item_img.color;
+        if (item_price <= DataController.Instance.haveGold)
+        {
+            temp.a = 0.9f;
+        }
+        else {
+            temp.a = 0.7f;
+        }
+
+        price_slider.value = DataController.Instance.haveGold;
+
+        item_img.color = temp;
+    }
+
+
+}
