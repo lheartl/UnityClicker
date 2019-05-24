0d368e6e97b8d7cc99dbbf10108bcd51d3d14936
lheartl <lheartl@nate.com>
2019-05-24 16:50:41 +0900

    아이템 버튼 제작

--- Assets/Scripts/BuyingItem.cs
+++ Assets/Scripts/BuyingItem.cs
@@ -7,56 +7,80 @@ public class BuyingItem : MonoBehaviour
 {
     public Slider price_slider;
     public CanvasGroup cGroup;
+
+    public Text priceText;
+    public Text autoIncreaseText;
+    public Text nextIncreaseText;
+
     [Header("ItemInfo Private")]
     public string itemName = "카타나";
     [SerializeField]
-    private int itemLevel = 1;
+    public int itemLevel = 1;
     [SerializeField]
     private int startItemPrice = 100;
     [SerializeField]
     private int autoIncreaseValue = 5;
 
-    [SerializeField]
-    private int nowItemPrice;
-    [SerializeField]
-    private int nowIncreaseValue;
-    [SerializeField]
-    private float increaseItemPrice = 1.35f;
-    [SerializeField]
-    private float increaseItemValue = 2.15f;
+    
+    public int nowItemPrice;
+    public int nowIncreaseValue;
+    public int beforeIncreaseValue;
+
+    public float increaseItemPrice = 2.75f;
+    
+    public float increaseItemValue = 1.35f;
 
     private void Start()
     {
+        
+
         price_slider.maxValue = startItemPrice;
         nowItemPrice = startItemPrice;
         nowIncreaseValue = autoIncreaseValue;
         StartCoroutine("AutoIncreaseWork");
+
+        priceText.text = "가격 : " + nowItemPrice;
+        autoIncreaseText.text = "현재 증가값 : 0";
+        nextIncreaseText.text = "다음 증가값 : " + nowIncreaseValue;
     }
     // Update is called once per frame
     void Update()
     {
         if (nowItemPrice <= DataController.Instance.haveGold)
         {
-            cGroup.alpha = 0.9f;
-            //cGroup.interactable = false;
+            
+            cGroup.interactable = true;
         }
         else {
-            cGroup.alpha = 0.6f;
-           // cGroup.interactable = true;
+           
+           cGroup.interactable = false;
         }
-
+        if (price_slider.normalizedValue < 0.2)
+        {
+            cGroup.alpha = 0.2f;
+        }
+        else {
+            cGroup.alpha = price_slider.normalizedValue;
+        }
+        
         price_slider.value = DataController.Instance.haveGold;
        
     }
 
     public void buyItem() {
         Debug.Log("클릭");
-        if (nowItemPrice <= DataController.Instance.haveGold) {
-            GameManager.Instance.useGold(nowItemPrice);
+        if (nowIncreaseValue <= DataController.Instance.haveGold) {
+            GameManager.Instance.useGold(nowIncreaseValue);
             itemLevel++;
-            nowItemPrice = startItemPrice *(int)Mathf.Pow(increaseItemPrice,itemLevel);
+
+            nowItemPrice = startItemPrice * (int)Mathf.Pow(increaseItemPrice, itemLevel);
+            beforeIncreaseValue = nowIncreaseValue;
             nowIncreaseValue = autoIncreaseValue * (int)Mathf.Pow(increaseItemValue, itemLevel);
             price_slider.maxValue = nowItemPrice;
+
+            priceText.text = "가격 : " + nowItemPrice;
+            autoIncreaseText.text = "초당 증가값 : " + beforeIncreaseValue;
+            nextIncreaseText.text = "다음 증가값 : " + nowIncreaseValue;
         }
         
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,13 +8,34 @@ public class GameManager : Singleton<GameManager>
     [Header("GameInfo Text")]
     public Text nowGoldText;
     public Text clickperGoldText;
+    public Text secondsperGoldText;
+    BuyingItem[] itemBtnList;
+
+    private void Start()
+    {
+        itemBtnList = FindObjectsOfType<BuyingItem>();
+    }
 
 
     void Update()
     {
+        
+        int tempIncrease = 0;
+        for (int i = 0; i <itemBtnList.Length; i ++) {
+            if (itemBtnList[i].itemLevel>1)
+            {
+                tempIncrease += itemBtnList[i].nowIncreaseValue;
+
+            }
+
+        }
+
+        DataController.Instance.autoIncreaseGold = tempIncrease;
+
         // 프레임마다 현재 보유 골드 표시
-        nowGoldText.text = "현재 보유 골드 : "+DataController.Instance.haveGold;
+        nowGoldText.text = "현재 보유 골드 : " + DataController.Instance.haveGold;
         clickperGoldText.text = "클릭당 골드 : " + DataController.Instance.clickPerGold;
+        secondsperGoldText.text = "초당 골드 : " + DataController.Instance.autoIncreaseGold;
 
     }
 
